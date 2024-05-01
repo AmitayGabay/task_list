32c1b5b85bbf68a1de3f5dfd584704bffd8b2dc6
Amitay <amitaygabay1@gmail.com>
2024-05-01 13:15:08 +0300

    add ajax

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const BASE_URL = "https://tasks-list.onrender.com/tasks";
-const _BASE_URL = "http://localhost:7000/tasks";
+$.ajaxSetup({
+    contentType: 'application/json'
+});
+const _BASE_URL = "https://tasks-list.onrender.com/tasks";
+// const _BASE_URL = "http://localhost:7000/tasks";
 
 const input = document.querySelector(".input");
 const add = document.createElement("button");
@@ -20,13 +23,27 @@ let itemsArr = [];
 let inputVal = "";
 input.addEventListener("change", (e) => {
     inputVal = e.target.value;
-    console.log(inputVal);
+    // console.log(inputVal);
 })
-document.querySelector("input").addEventListener("change", (e) => console.log(e))
+// document.querySelector("input").addEventListener("change", (e) => console.log(e))
 const getDataFromApi = async () => {
-    itemsArr = await (await fetch(`${BASE_URL}`)).json();
-    itemsArr.reverse();
-    renderTodo();
+    // itemsArr = await (await fetch(`${_BASE_URL}`)).json();
+    // itemsArr.reverse();
+    // renderTodo();
+    // $.get(`${_BASE_URL}`)
+    //     .done(function (data) {
+    //         itemsArr = data;
+    //         itemsArr.reverse();
+    //         renderTodo();
+    //     })
+    //     .fail(function (xhr, status, error) {
+    //         console.error('Error:', error);
+    //     });
+    $.get(`${_BASE_URL}`, {}, function (data) {
+        itemsArr = data;
+        itemsArr.reverse();
+        renderTodo();
+    })
 }
 
 getDataFromApi();
@@ -42,39 +59,87 @@ input.addEventListener("keyup", () => {
 
 input.addEventListener("keypress", async (e) => {
     if (e.key == "Enter" && input.value.trim().length) {
-        try {
-            const data = await (await fetch(`${BASE_URL}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ content: input.value })
-            })).json();
+        // try {
+        //     const data = await (await fetch(`${_BASE_URL}`, {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({ content: input.value })
+        //     })).json();
+        //     itemsArr.unshift(data);
+        //     add.style.display = "none";
+        //     input.value = "";
+        //     renderTodo();
+        // }
+        // catch (e) { console.error(e) };
+
+        // $.ajax({
+        //     url: `${_BASE_URL}`,
+        //     method: 'POST',
+        //     contentType: 'application/json',
+        //     data: JSON.stringify({ content: input.value }),
+        //     success: function (data) {
+        //         itemsArr.unshift(data);
+        //         add.style.display = "none";
+        //         input.value = "";
+        //         renderTodo();
+        //     },
+        //     error: function (xhr, status, error) {
+        //         console.error('Error:', error);
+        //     }
+        // });
+        $.post(_BASE_URL, JSON.stringify({ content: input.value }), function (data) {
             itemsArr.unshift(data);
             add.style.display = "none";
             input.value = "";
             renderTodo();
-        }
-        catch (e) { console.error(e) };
+        }, 'json').fail(function (xhr, status, error) {
+            console.error('Error:', error);
+        });
     }
 })
 
 add.addEventListener("click", async () => {
     if (input.value.trim().length) {
-        try {
-            const data = await (await fetch(`${BASE_URL}`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ content: input.value })
-            })).json();
+        // try {
+        //     const data = await (await fetch(`${_BASE_URL}`, {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json'
+        //         },
+        //         body: JSON.stringify({ content: input.value })
+        //     })).json();
+        //     itemsArr.unshift(data);
+        //     add.style.display = "none";
+        //     input.value = "";
+        //     renderTodo();
+        // }
+        // catch (e) { console.error(e) };
+
+        // $.ajax({
+        //     url: `${_BASE_URL}`,
+        //     method: 'POST',
+        //     contentType: 'application/json',
+        //     data: JSON.stringify({ content: input.value }),
+        //     success: function (data) {
+        //         itemsArr.unshift(data);
+        //         add.style.display = "none";
+        //         input.value = "";
+        //         renderTodo();
+        //     },
+        //     error: function (xhr, status, error) {
+        //         console.error('Error:', error);
+        //     }
+        // });
+        $.post(_BASE_URL, JSON.stringify({ content: input.value }), function (data) {
             itemsArr.unshift(data);
             add.style.display = "none";
             input.value = "";
             renderTodo();
-        }
-        catch (e) { console.error(e) };
+        }, 'json').fail(function (xhr, status, error) {
+            console.error('Error:', error);
+        });
     }
 })
 
@@ -127,17 +192,32 @@ const removeItem = (_remove_btn) => {
             })
             let toggle = itemsArr[indexOfItemDeleted].isDone;
 
-            await fetch(`${BASE_URL}?id=${itemsArr[indexOfItemDeleted]._id}`, {
-                method: "DELETE"
+            // await fetch(`${_BASE_URL}?id=${itemsArr[indexOfItemDeleted]._id}`, {
+            //     method: "DELETE"
+            // });
+            // itemsArr.splice(indexOfItemDeleted, 1);
+            // if (toggle === false) {
+            //     renderTodo();
+            // }
+            // else {
+            //     renderDone();
+            // }
+            $.ajax({
+                url: `${_BASE_URL}?id=${itemsArr[indexOfItemDeleted]._id}`,
+                method: 'DELETE',
+                success: function () {
+                    itemsArr.splice(indexOfItemDeleted, 1);
+                    if (toggle === false) {
+                        renderTodo();
+                    }
+                    else {
+                        renderDone();
+                    }
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                }
             });
-            itemsArr.splice(indexOfItemDeleted, 1);
-
-            if (toggle === false) {
-                renderTodo();
-            }
-            else {
-                renderDone();
-            }
         } catch (e) {
             console.error(e);
         }
@@ -153,26 +233,52 @@ const doneItem = (_done_btn) => {
             let toggle = itemsArr[indexOfItemDone].isDone;
 
             if (toggle === false) {
-                await fetch(`${BASE_URL}/done`, {
-                    method: "PUT",
-                    headers: {
-                        'Content-Type': 'application/json'
+                // await fetch(`${_BASE_URL}/done`, {
+                //     method: "PUT",
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id })
+                // });
+                // itemsArr[indexOfItemDone].isDone = true;
+                // renderTodo();
+                $.ajax({
+                    url: `${_BASE_URL}/done`,
+                    method: 'PUT',
+                    contentType: 'application/json',
+                    data: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id }),
+                    success: function () {
+                        itemsArr[indexOfItemDone].isDone = true;
+                        renderTodo();
                     },
-                    body: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id })
+                    error: function (xhr, status, error) {
+                        console.error('Error:', error);
+                    }
                 });
-                itemsArr[indexOfItemDone].isDone = true;
-                renderTodo();
             }
             else {
-                await fetch(`${BASE_URL}/undone`, {
-                    method: "PUT",
-                    headers: {
-                        'Content-Type': 'application/json'
+                // await fetch(`${_BASE_URL}/undone`, {
+                //     method: "PUT",
+                //     headers: {
+                //         'Content-Type': 'application/json'
+                //     },
+                //     body: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id })
+                // });
+                // itemsArr[indexOfItemDone].isDone = false;
+                // renderDone();
+                $.ajax({
+                    url: `${_BASE_URL}/undone`,
+                    method: 'PUT',
+                    contentType: 'application/json',
+                    data: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id }),
+                    success: function () {
+                        itemsArr[indexOfItemDone].isDone = false;
+                        renderDone();
                     },
-                    body: JSON.stringify({ _id: itemsArr[indexOfItemDone]._id })
+                    error: function (xhr, status, error) {
+                        console.error('Error:', error);
+                    }
                 });
-                itemsArr[indexOfItemDone].isDone = false;
-                renderDone();
             }
         } catch (e) {
             console.error(e);
@@ -232,18 +338,40 @@ todo.addEventListener("click", () => {
 remove_all.addEventListener("click", async () => {
     try {
         if (spanRemove_all.innerText === "TODO") {
-            await fetch(`${BASE_URL}/todo-list`, {
-                method: "DELETE"
+            // await fetch(`${_BASE_URL}/todo-list`, {
+            //     method: "DELETE"
+            // });
+            // itemsArr = itemsArr.filter(item => item.isDone !== false);
+            // renderTodo();
+            $.ajax({
+                url: `${_BASE_URL}/todo-list`,
+                method: 'DELETE',
+                success: function () {
+                    itemsArr = itemsArr.filter(item => item.isDone !== false);
+                    renderTodo();
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                }
             });
-            itemsArr = itemsArr.filter(item => item.isDone !== false);
-            renderTodo();
         }
         if (spanRemove_all.innerText === "DONE") {
-            await fetch(`${BASE_URL}/done-list`, {
-                method: "DELETE"
+            // await fetch(`${_BASE_URL}/done-list`, {
+            //     method: "DELETE"
+            // });
+            // itemsArr = itemsArr.filter(item => item.isDone !== true);
+            // renderDone();
+            $.ajax({
+                url: `${_BASE_URL}/done-list`,
+                method: 'DELETE',
+                success: function () {
+                    itemsArr = itemsArr.filter(item => item.isDone !== true);
+                    renderDone();
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                }
             });
-            itemsArr = itemsArr.filter(item => item.isDone !== true);
-            renderDone();
         }
     } catch (e) {
         console.error(e);
